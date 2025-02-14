50eb46f945d494fda0627ee7b177c1e6c9d93018
PeteChu <phuengton.ch@gmail.com>
2025-02-14 11:09:57 +0700

    feat(config): add check_origin settings for RequestBinWeb.Endpoint

--- config/prod.exs
+++ config/prod.exs
@@ -6,6 +6,7 @@ import Config
 # which you should run after static files are built and
 # before starting your production server.
 config :request_bin, RequestBinWeb.Endpoint,
+  check_origin: ["//*.petechu.dev", "//*.fly.dev"],
   cache_static_manifest: "priv/static/cache_manifest.json"
 
 # Configures Swoosh API Client
